434ebd5c9c8f4dd489c3a575d9d426b8b9fa5373
Leo.Hu <lalalaleo.hu@hotmail.com>
2017-06-09 23:14:45 +0800

    排行榜，待完善

--- App/server/gamePoint.js
+++ App/server/gamePoint.js
@@ -17,9 +17,13 @@ exports.add = function(username,gamepoint,fn){
   });
 }
 
-//   var d = new Date();
-//   var sql = 'INSERT INTO gamepoint (gb_username,gb_num,gb_time) VALUES ( "'+username+'", "'+gamepoint+'","'+d.toLocaleString()+'")';
-//   var sql = 'INSERT INTO gamepoint (gb_username,gb_num,gb_time) VALUES ( "'+username+'", "'+gamepoint+'","'+d.toLocaleString()+'")';
-//   connection.query('INSERT INTO gamepoint (gp_username,gp_num,gp_time) VALUES ( "Test", 18,"2017-06-09 22.54.00")', function (error, results, fields) {
-//     if (error) throw error;
-//   });
+exports.getTop = function(){
+    var sql = 'SELECT * FROM gamepoint ORDER BY gp_num DESC,gp_time limit 10';
+    connection.query(sql, function (error, results, fields) {
+    if (error) throw error;
+    else {
+        
+    }
+    return 0;
+  });
+}
